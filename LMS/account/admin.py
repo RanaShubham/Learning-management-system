b4c516896cb6c1f5c35d6c0c11e1c42f4c878949
Shubham Rana <ranashubham7296@gmail.com>
2021-01-20 15:42:22 +0530

    [Shubham Rana] Refactored: To use custom exceptions.

--- LMS/account/admin.py
+++ LMS/account/admin.py
@@ -1,8 +1,4 @@
 from django.contrib import admin
-from .models import User as user
-# from django.contrib.auth import get_user_model
-
-# Register your models here.
-User = user
+from .models import User
 
 admin.site.register(User)
--- LMS/account/models.py
+++ LMS/account/models.py
@@ -1,17 +1,24 @@
-from django.db import models
+from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.db import models
+from LMS.utils import ExceptionType, LMSException
+
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password, is_active=True, name = None, role = None):
+    def create_user(self, email, password, is_active=True, name=None, role=None):
         '''
             Method to create a user record for the database.
             Throws vlaue error if no email is provided.
             Throws value error if no password is provided.
         '''
+        if not name:
+            raise LMSException(ExceptionType, "User must have a name.")
+        if not role:
+            raise LMSException(ExceptionType, "User must have a role.")
         if not email:
-            raise ValueError("User must have an email.")
+            raise LMSException(ExceptionType.UserException, "User must have an email.")
         if not password:
-            raise ValueError("User must have a password.")
+            raise LMSException(ExceptionType.UserException, "User must have a password.")
 
         user_obj = self.model(
             email = self.normalize_email(email),
@@ -29,6 +36,7 @@ class User(AbstractBaseUser):
     phone_number = models.CharField(max_length=10, blank=False, null=False)
     role = models.CharField(max_length=16, null=False, blank=False)
     is_deleted = models.BooleanField(default=False)
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
     objects = UserManager()
