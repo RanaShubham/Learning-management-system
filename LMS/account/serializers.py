41b52c348705d680e8c2901dab79672bdc3e2e22
anamfazal <anamfazal94@gmail.com>
2021-01-21 13:49:08 +0530

    [Anam Fazal]:added login views

--- LMS/account/serializers.py
+++ LMS/account/serializers.py
@@ -1,6 +1,8 @@
+from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
 from .models import User
+from django.contrib import auth
 
 class RegisterSerializer(ModelSerializer):
     # password = serializers.CharField(
@@ -11,7 +13,7 @@ class RegisterSerializer(ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['email', 'name', 'role','phone_number', ]#'password'
+        fields = ['email', 'name', 'role','phone_number',]#'password'
 
     # def validate(self, attrs):
     #     email = attrs.get('email', '')
@@ -24,3 +26,49 @@ class RegisterSerializer(ModelSerializer):
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
+
+
+class LoginSerializer(ModelSerializer):
+    """[validates user credentials and allows login if authenticated]
+
+    Raises:
+        AuthenticationFailed: [if improper credentials are passed]
+        AuthenticationFailed: [if user account is not active]
+
+    Returns:
+        [string]: [email]
+    """
+
+
+
+    email = serializers.EmailField(max_length=255, min_length=3)
+    password = serializers.CharField(
+        max_length=68, min_length=3, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'password']
+
+    def validate(self, attrs):
+        """
+        it verifies the credentials, if credentials were matched then returns data in json format, else throws exception
+        :return: return json data if credentials are matched
+        """
+        email = attrs.get('email', '')
+        password = attrs.get('password', '')
+
+        print(password)
+        user = auth.authenticate(email=email, password=password)
+        print(user.email)
+        print(user.password)
+
+        if not user:
+            raise AuthenticationFailed('Invalid credentials, please try again')
+        if not user.is_active:
+            raise AuthenticationFailed('Account disabled, contact admin')
+
+        return {
+            'email': user.email,
+        }
+
+
--- LMS/account/views.py
+++ LMS/account/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render
+from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .dummy_data import get_response
-from .utils import Util, get_random_password
-from .serializers import RegisterSerializer
+from .utils import Util
+from .serializers import RegisterSerializer,LoginSerializer
 from .models import *
+from rest_framework import serializers
+
+import datetime
 # from .models import UserManager
 
 # Create your views here.
 class GetUser(APIView):
     def get(self , request, **kwargs):
-        return Response({'status':True, "message":get_response}, status=status.HTTP_200_OK)
+        user=User.objects.filter(email='asdfgh@gmail.com').first()
+        return Response({'status':True, "message":user}, status=status.HTTP_200_OK)
 
 class RegisterUser(APIView):
     serializer_class = RegisterSerializer
@@ -38,7 +43,7 @@ class RegisterUser(APIView):
         # print(get_random_password())
         # print(password2)
         user = User.objects.get(email=user_data['email'], name=user_data['name'], phone_number=user_data['phone_number'], role=user_data['role'])
-        # print("aaya")
+        print(user)
         Util.send_email(user)
         response = {'status':True,'message':'Registered successfully. Login Crdentials have been sent to your email.'}
         return Response(response, status=status.HTTP_200_OK)
@@ -50,7 +55,47 @@ class UpdateUser(APIView):
         return Response(response, status=status.HTTP_200_OK)
 
 class LoginUser(APIView):
-    def post(self, request, *args, **kwargs):
-        #TODO: LOGIN USER HERE.
-        response = {'status':True,'message':'Logged in successfully'}
-        return Response(response, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        """[validates user email and password, sets user id in cache]
+
+        :param request:[mandatory]:[string]:email of user
+                                   [string]:password
+        :return: [dictionary]:status[boolean]
+                              response message[string]
+                              token[string]
+                 [int]:status code
+        """
+        try:
+            serializer = LoginSerializer(data=request.data)
+            print('serialized')
+            serializer.is_valid(raise_exception=True)
+            print(serializer.errors)
+            data=serializer.data
+            print(data)
+            user = User.objects.filter(email=data['email']).first()
+            print(user)
+            current_time = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+            #token = Encrypt.encode(user.id,current_time)
+            #cache.set("TOKEN_"+str(user.id)+"_AUTH", token)
+            #result = utils.manage_response(status=True, message='Token generated', log='successfully logged in', logger_obj=logger)
+            result = {'status':True,'message':' Login successful.'}
+            response = Response(result, status=status.HTTP_200_OK,content_type="application/json")
+            #response.__setitem__(header="HTTP_AUTHORIZATION",value=token)
+            return response
+
+        except serializers.ValidationError as se:
+            #logging.warning(se)
+            response = {'message':str(se.detail), 'status':False}
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+        except User.DoesNotExist as e:
+            result = {'status':False,'message':str(e)}
+            return Response(result, status.HTTP_400_BAD_REQUEST,content_type="application/json")
+        except AuthenticationFailed as e:
+            result ={'status':False,'message':str(e)}
+
+            return Response(result, status.HTTP_400_BAD_REQUEST, content_type="application/json")
+        except Exception as e:
+            result = {'status':False,'message':str(e)}
+            return Response(result, status.HTTP_400_BAD_REQUEST,content_type="application/json")
