c3da7a080194857fbcb5f5894116f125517bacbb
TarunVyda6 <kamaltarun.rao6@gmail.com>
2021-01-22 14:33:00 +0530

    [Tarun] Resolved : Merge conflicts

--- LMS/student/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('student/', views.StudentDetails.as_view(), name="student-register"),
-    path('student/<int:pk>/', views.StudentDetails.as_view(), name="student-details"),
-]
