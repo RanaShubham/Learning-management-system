2c6602209a61cd4f0c5a3307fdbe3a97b549a6b9
Shubham Rana <ranashubham7296@gmail.com>
2021-01-24 20:15:46 +0530

    [Shubham Rana] Added: Roles model.

--- LMS/account/models.py
+++ LMS/account/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.db.models.fields import CharField, IntegerField
 from django.http import HttpResponse
 from rest_framework import status
 import json
@@ -24,7 +25,7 @@ class UserManager(BaseUserManager):
             raise LMSException(ExceptionType.UserException,
                 'Superuser must be assigned to is_superuser=True.')
 
-        return self.create_user(name=name, email=email,role='admin',
+        return self.create_user(name=name, email=email, role=Role.get_role_admin(),
                                 password=password,phone_number=phone_number, **other_fields)
 
     def create_user(self, email, name, role,phone_number,password=get_random_password(), **other_fields):
@@ -55,11 +56,33 @@ class UserManager(BaseUserManager):
         return user
 
 
+class Role(models.Model):
+    """[Role id and role name mapping providee by this table.]
+
+    Args:
+        models ([models.Model]): [Inherits Model]
+    """
+    role_id = IntegerField(blank=False, null=False, unique=True)
+    role = CharField(max_length=64, blank=False, null=False, unique=True)
+
+    def __str__(self):
+        return self.role
+
+    @staticmethod
+    def get_role_admin():
+        if not Role.objects.filter(role='admin').first():
+            Role(role_id = 1, role = 'admin').save()
+        return Role.objects.filter(role='admin').first()
+
+         
 class User(AbstractBaseUser, PermissionsMixin):
+    """[User model for diffrent type of users.]
+    """
     email = models.EmailField(max_length=128, unique=True)
     name = models.CharField(max_length=32, blank=False, null=False)
     phone_number = models.CharField(max_length=10, blank=False, null=False)
-    role = models.CharField(max_length=16, null=False, blank=False)
+    # role = models.CharField(max_length=16, null=False, blank=False)
+    role = models.ForeignKey('Role', on_delete=models.CASCADE)
     is_deleted = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=False)
@@ -80,4 +103,3 @@ class User(AbstractBaseUser, PermissionsMixin):
     def soft_delete(self):
         self.is_deleted = True
         self.save()
-
