dfed548d6c9979bec9df3ac8afabb30c2fba0486
anamfazal <anamfazal94@gmail.com>
2021-01-25 10:29:53 +0530

    [Anam Fazal]Added:logging and using manage response utility for responses

--- LMS/account/views.py
+++ LMS/account/views.py
@@ -1,9 +1,7 @@
 import datetime
-from django.urls import reverse
-from django.contrib.auth.tokens import PasswordResetTokenGenerator
-from django.utils.encoding import smart_bytes
-from django.utils.http import urlsafe_base64_encode
-from .serializers import LoginSerializer,RegisterSerializer,ResetPasswordEmailRequestSerializer,SetNewPasswordSerializer
+import logging
+import os
+from .serializers import LoginSerializer,RegisterSerializer
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -16,6 +14,17 @@ from services.cache import Cache
 from .utils import Util
 from rest_framework import serializers
 from LMS.utils import ExceptionType, LMSException
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s  %(name)s  %(levelname)s: %(message)s')
+
+file_handler = logging.FileHandler(os.path.abspath("loggers/log_accounts.log"))
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+
 
 
 @method_decorator(user_login_required, name='dispatch')
@@ -34,17 +43,18 @@ class RegisterUser(APIView):
 
             users = User.objects.filter(is_deleted=False)
             serializer = RegisterSerializer(users, many=True)
-            response = {'status': True,
-                        'message': 'Retrieved all users.','data':serializer.data}
+            response = Util.manage_response(status=True, message='Retrieved all users.', data=serializer.data,
+                                           log='retrieved users', logger_obj=logger)
             return Response(response, status=status.HTTP_200_OK)
         except LMSException as e:
-            result = {'status': False, 'message': e.message}
-            return Response(result, status.HTTP_404_NOT_FOUND, content_type="application/json")
-        except User.DoesNotExist:
-            response = {'status': False, 'message': 'Please login again.'}
-            return Response(response, status=status.HTTP_400_BAD_REQUEST)
-        except Exception:
-            response = {'status':False, 'message':'Somethign went wrong.'}
+            response = Util.manage_response(status=False,
+                                            message=e.message,
+                                            log=e.message, logger_obj=logger)
+            return Response(response, status.HTTP_404_NOT_FOUND, content_type="application/json")
+        except Exception as e:
+            response = Util.manage_response(status=False,
+                                            message="Something went wrong.Please try again",
+                                            log=str(e), logger_obj=logger)
             return Response(response, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     def post(self, request, **kwargs):
@@ -67,15 +77,27 @@ class RegisterUser(APIView):
             user = User.objects.get(email=user_data['email'], name=user_data['name'],
                                     phone_number=user_data['phone_number'], role=user_data['role'])
             Util.send_email(user)
-            response = {'status': True,
-                        'message': 'Registered successfully. Login Credentials have been sent to your email.'}
+            response = Util.manage_response(status=True, message='Registered successfully.Login Credentials have been sent to your email.',
+                                            log='Registered successfully.Login credentials sent.', logger_obj=logger)
             return Response(response, status=status.HTTP_200_OK)
+        except LMSException as e:
+            response = Util.manage_response(status=False,
+                                            message=e.message,
+                                            log=e.message, logger_obj=logger)
+            return Response(response, status.HTTP_404_NOT_FOUND, content_type="application/json")
+
         except serializers.ValidationError as e:
-            result = {'status': False, 'message': e.detail}
-            return Response(result, status.HTTP_400_BAD_REQUEST, content_type="application/json")
+            response = Util.manage_response(status=False,
+                                            message=e.detail,
+                                            log=e.detail, logger_obj=logger)
+
+            return Response(response, status.HTTP_400_BAD_REQUEST, content_type="application/json")
         except Exception as e:
-            result = {'status': False, 'message': 'Some other issue.Please try again.'}
-            return Response(result, status.HTTP_400_BAD_REQUEST, content_type="application/json")
+            response = Util.manage_response(status=False,
+                                            message="Something went wrong.Please try again",
+                                            log=str(e), logger_obj=logger)
+
+            return Response(response, status.HTTP_400_BAD_REQUEST, content_type="application/json")
 
 
 
@@ -98,15 +120,22 @@ class RegisterUser(APIView):
             if serializer.is_valid(raise_exception=True):
                 serializer.save()
             user_data = serializer.data
-            response = {'status': True,
-                        'message': 'Updated successfully.','data':user_data}
+            response = Util.manage_response(status=True,
+                                            message='Updated successfully.',data=user_data,
+                                            log='Updated user record successfully.', logger_obj=logger)
+
             return Response(response, status=status.HTTP_200_OK)
         except LMSException as e:
-            result = {'status': False, 'message': e.message}
-            return Response(result, status.HTTP_404_NOT_FOUND, content_type="application/json")
+            response = Util.manage_response(status=False,
+                                            message=e.message,
+                                            log=e.message, logger_obj=logger)
+            return Response(response, status.HTTP_404_NOT_FOUND, content_type="application/json")
         except Exception as e:
-            result = {'status': False, 'message': 'Some other issue.Please try again'}
-            return Response(result, status.HTTP_400_BAD_REQUEST, content_type="application/json")
+            response = Util.manage_response(status=False,
+                                            message="Something went wrong.Please try again",
+                                            log=str(e), logger_obj=logger)
+
+            return Response(response, status.HTTP_400_BAD_REQUEST, content_type="application/json")
 
 
     def delete(self, request, pk, **kwargs):
@@ -125,16 +154,23 @@ class RegisterUser(APIView):
             else:
                 user = User.objects.get(id=pk)
                 user.soft_delete()
-            response = {'status': True,
-                        'message': 'Deleted successfully.'}
+            response = Util.manage_response(status=True,
+                                            message='Deleted successfully.',
+                                            log='Deleted user record successfully.', logger_obj=logger)
             return Response(response, status=status.HTTP_200_OK)
 
         except LMSException as e:
-            result = {'status': False, 'message': e.message}
-            return Response(result, status.HTTP_404_NOT_FOUND, content_type="application/json")
+            response = Util.manage_response(status=False,
+                                            message=e.message,
+                                            log=e.message, logger_obj=logger)
+
+            return Response(response, status.HTTP_404_NOT_FOUND, content_type="application/json")
         except Exception as e:
-            result = {'status': False, 'message': 'Some other issue.Please try again'}
-            return Response(result, status.HTTP_400_BAD_REQUEST, content_type="application/json")
+            response = Util.manage_response(status=False,
+                                            message="Something went wrong.Please try again",
+                                            log=str(e), logger_obj=logger)
+
+            return Response(response, status.HTTP_400_BAD_REQUEST, content_type="application/json")
 
 
 class LoginUser(APIView):
@@ -152,19 +188,31 @@ class LoginUser(APIView):
             current_time = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
             token = Encrypt.encode(user.id, current_time)
             Cache.getInstance().set("TOKEN_" + str(user.id) + "_AUTH", token)
-            result = {'status':True, 'message':'Token generated.Login successful.'}
+            result = Util.manage_response(status=True,
+                                            message='Token generated.Login successful.',
+                                            log='Token generated.Login successful.', logger_obj=logger)
+
             response = Response(result, status=status.HTTP_200_OK, content_type="application/json")
             response.__setitem__(header="HTTP_AUTHORIZATION", value=token)
             return response
         except User.DoesNotExist as e:
-            result = {'status': False, 'message': 'User does not exist'}
+            response = Util.manage_response(status=False,
+                                            message="User does not exist",
+                                            log=str(e), logger_obj=logger)
+
             return Response(result, status.HTTP_400_BAD_REQUEST, content_type="application/json")
         except AuthenticationFailed as e:
-            result = {'status': False, 'message': 'Invalid credentials'}
-            return Response(result, status.HTTP_401_UNAUTHORIZED, content_type="application/json")
+            response = Util.manage_response(status=False,
+                                            message="Invalid credentials",
+                                            log=str(e), logger_obj=logger)
+
+            return Response(response, status.HTTP_401_UNAUTHORIZED, content_type="application/json")
         except Exception as e:
-            result = {'status': False, 'message': 'Some other issue.Please try again'}
-            return Response(result, status.HTTP_400_BAD_REQUEST, content_type="application/json")
+            response = Util.manage_response(status=False,
+                                            message="Something went wrong.Please try again",
+                                            log=str(e), logger_obj=logger)
+
+            return Response(response, status.HTTP_400_BAD_REQUEST, content_type="application/json")
 
     @method_decorator(user_login_required, name='dispatch')
     def get(self, request, **kwargs):
@@ -178,76 +226,13 @@ class LoginUser(APIView):
             current_user = kwargs['userid']
             if cache.get("TOKEN_" + str(current_user) + "_AUTH"):
                 cache.delete("TOKEN_" + str(current_user) + "_AUTH")
+            response = Util.manage_response(status=True,
+                                          message='Logged out',
+                                          log='Logged out', logger_obj=logger)
 
-            result={'status':True, 'message':'Logged out'}
-
-            return Response(result, status=status.HTTP_200_OK, content_type="application/json")
+            return Response(response, status=status.HTTP_200_OK, content_type="application/json")
         except Exception as e:
-            result = {'status': True, 'message': 'some other issue.Please try again'}
-            return Response(result, status.HTTP_400_BAD_REQUEST, content_type="application/json")
-
-
-
-
-
-
-
-
-#TODO:url-> 1.send reset email 2.url slugfield(as arg) connected with another view
-class RequestPasswordResetEmail(APIView):
-    """[sends an email to facilitate password reset]
-    """
-    serializer_class = ResetPasswordEmailRequestSerializer
-
-    def post(self, request,**kwargs):
-        """[sends an email to facilitate password reset]
-
-        :param request: [mandatory]:[string]:email of user
-        :return: [string] confirmation message
-                 email with link to reset password
-                 [int] status code
-        """
-        try:
-            email = request.data.get('email', '')
-
-            if User.objects.filter(email=email).exists():
-                user = User.objects.get(email=email)
-                uidb64 = urlsafe_base64_encode(smart_bytes(user.id))
-                token = PasswordResetTokenGenerator().make_token(user)
-                #redirect_url = request.build_absolute_uri(reverse("password-reset-complete"))
-                redirect_url = 'http://127.0.0.1:8000/users/reset-password-complete/' #TODO:try to user reverse
-
-                email_body = 'Hello, \n Your token number is : ' + token + ' \n your uidb64 code is ' + uidb64 + ' \n Use link below to reset your password  \n' + "?redirect_url=" + redirect_url
-                data = {'email_body': email_body, 'to_email': user.email,'email_subject': 'Reset your passsword'}
-                Util.send_reset_email(data)
-                result = {'status':True ,'message':'We have sent you a link to reset your password' }
-                return Response(result, status=status.HTTP_200_OK, content_type="application/json")
-
-            else:
-                result = {'status': False, 'message': "Email id you have entered doesn't exist"}
-                return Response(result, status=status.HTTP_400_BAD_REQUEST, content_type="application/json")
-
-
-        except Exception as e:
-            result = {'status': False, 'message': 'Something went wrong.Please try again.'}
-            return Response(result, status=status.HTTP_400_BAD_REQUEST, content_type="application/json")
-
-
-class SetNewPassword(APIView):
-    """[returns new password when supplied with uid,token and new password]
-    """
-    serializer_class = SetNewPasswordSerializer
-
-    def patch(self, request,**kwargs):
-        """[returns new password when supplied with uid,token and new password]
-
-        :param request: [mandatory]:[string]: new password
-        :return: confirmation message and status code
-        """
-
-        serializer = self.serializer_class(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        result = {'status': True, 'message': 'Password reset successful'}
-        return Response(result, status=status.HTTP_200_OK, content_type="application/json")
-
-
+            response = Util.manage_response(status=False,
+                                            message="Something went wrong.Please try again",
+                                            log=str(e), logger_obj=logger)
+            return Response(response, status.HTTP_400_BAD_REQUEST, content_type="application/json")
