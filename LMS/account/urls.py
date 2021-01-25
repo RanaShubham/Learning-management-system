418fde71af4970fdab1cd06eb8a57b1993215154
anamfazal <anamfazal94@gmail.com>
2021-01-25 10:27:50 +0530

    [Anam Fazal]Refactored:Unnecessary paths removed

--- LMS/account/urls.py
+++ LMS/account/urls.py
@@ -10,7 +10,5 @@ urlpatterns = [
     re_path(r'^(?P<pk>[0-9]{0,})$', RegisterUser.as_view(), name="register_get_update_delete"),
     path('login', LoginUser.as_view(), name="login_user"),
     path('logout', LoginUser.as_view(), name="logout_user"),
-    path('request-reset-email/', RequestPasswordResetEmail.as_view(),name="request-reset-email"),
-    path('reset-password-complete/', SetNewPassword.as_view(),name="password-reset-complete"),
-]
+   ]
 
