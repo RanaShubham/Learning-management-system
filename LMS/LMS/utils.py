bd58ed1fb921ada6ec9dec301ae5de93668a95e8
anamfazal <anamfazal94@gmail.com>
2021-01-25 10:29:00 +0530

    [Anam Fazal]Added:enums for student app exception handling

--- LMS/LMS/utils.py
+++ LMS/LMS/utils.py
@@ -4,6 +4,9 @@ class ExceptionType(enum.Enum):
     UserException = "Cannot create user instance."
     NonExistentError = "Requested user does not exist"
     UnauthorizedError = "Sorry,you are not authorized to perform this operation."
+    StudentNotFound = "student data not found"
+    StudentExist = "student already exist"
+
 
 class LMSException(Exception):
     """[Custom exception.]
