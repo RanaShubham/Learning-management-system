8ddb763397c2dadb860536cc8d9c51fe22cdfa84
anamfazal <anamfazal94@gmail.com>
2021-01-27 22:08:15 +0530

    [Anam Fazal]Refactored:course field in mentor model to be ForeignKey

--- LMS/mentor/models.py
+++ LMS/mentor/models.py
@@ -4,8 +4,8 @@ from course.models import Course
 
 
 class Mentor(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='mentor', null=True)
-    course = models.ManyToManyField(Course, related_name='courses',blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='mentor')
+    course = models.ForeignKey(Course,on_delete=models.CASCADE, related_name='course')
     is_deleted = models.BooleanField(default=False)
 
     def __str__(self):
