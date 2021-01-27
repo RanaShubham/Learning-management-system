a4f68de653201529bbc85eb13a1f8bb29922c835
anamfazal <anamfazal94@gmail.com>
2021-01-27 16:32:54 +0530

    [Anam Fazal]Added:mentor app urls

--- LMS/mentor/urls.py
+++ LMS/mentor/urls.py
@@ -3,5 +3,5 @@ from .views import *
 
 urlpatterns = [
     re_path(r'^(?P<pk>[0-9]{0,})$', AdminView.as_view(), name="admin_get"),
-    path('profile/',MentorProfile.as_view(),name="get"),
+    path('profile/',MentorProfile.as_view(),name="get-post"),
 ]
