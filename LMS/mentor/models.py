c480cef65d5fcd1aed19298fa3104472644a5581
anamfazal <anamfazal94@gmail.com>
2021-01-27 16:29:55 +0530

    [Anam Fazal]Refactored:added is_deleted  and soft delete method in model

--- LMS/mentor/models.py
+++ LMS/mentor/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from account.models import User
+from course.models import Course
 
 
 class Mentor(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='mentor', null=True)
-    course = models.CharField(max_length=50, blank=False, null=False)
-    #TODO:course = models.ForeignKey(Course,on_delete=models.CASCADE, related_name='course', null=True)
+    course = models.ManyToManyField(Course, related_name='courses',blank=True)
+    is_deleted = models.BooleanField(default=False)
 
     def __str__(self):
         return self.user.name
+
+
+    def soft_delete(self):
+        self.is_deleted = True
+        self.save()
+
