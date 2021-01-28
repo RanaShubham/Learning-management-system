b0b6949092c8683199e77e059e512a8e9c14afa8
IbraheemKhaleel <ikhaleelem@gmail.com>
2021-01-28 11:03:09 +0530

    [Ibraheem Khaleel]Refactored: code for creating neagative test cases

--- LMS/student/tests/test_views.py
+++ LMS/student/tests/test_views.py
@@ -63,24 +63,27 @@ class Data(TestCase):
         self.valid_patch_data = {
             "alternate_contact_number": "9517"
         }
-
-
-class StudentDetailsTest(Data):
-
-    def test_student_with_valid_details(self):
+        self.invalid_student_form = {
+            "alternate_contact_number": "9517538",
+            "relationship_of_alternate_contact_person": "Parent",
+        }
         user = User.objects.create_superuser(name='admin', email='adminpass@gmail.com', phone_number='1234',
                                              password='adminpassword')
-        print(user.name)
+
         role = Role.objects.create(role_id=2, role='student')
-        print(role.role)
-        print(user.name)
+
         response = self.client.post(self.admin_login_url, self.admin_login_data, format='application/json')
         headers = response.__getitem__(header="HTTP_AUTHORIZATION")
         self.client.post(self.admin_register_user, self.admin_register_user_data, HTTP_AUTHORIZATION=headers,
                          format='application/json ')
         password = Cache.getInstance().get("TOKEN_" + "password" + "_AUTH").decode("utf-8")
-        print(password)
         self.student_login_data['password'] = password
+
+
+class StudentDetailsTest(Data):
+
+    def test_student_with_valid_details(self):
+
         response = self.client.post(self.admin_login_url, self.student_login_data, format='application/json')
         headers = response.__getitem__(header="HTTP_AUTHORIZATION")
         response = self.client.post(self.student_register_url, self.valid_student_form, HTTP_AUTHORIZATION=headers,
@@ -93,3 +96,10 @@ class StudentDetailsTest(Data):
         response = client.patch(self.single_student_url, self.valid_patch_data, HTTP_AUTHORIZATION=headers,
                                 format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_student_with_invalid_details(self):
+        response = self.client.post(self.admin_login_url, self.student_login_data, format='application/json')
+        headers = response.__getitem__(header="HTTP_AUTHORIZATION")
+        response = self.client.post(self.student_register_url, self.invalid_student_form, HTTP_AUTHORIZATION=headers,
+                                    format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
