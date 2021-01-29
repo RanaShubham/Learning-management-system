f06fdf4fcafbe07366a2a75e60b8a554fff0c4c7
anamfazal <anamfazal94@gmail.com>
2021-01-29 12:17:58 +0530

    [Anam Fazal]Refactored:added test to check soft delete function

--- LMS/account/tests/test_account_models.py
+++ LMS/account/tests/test_account_models.py
@@ -8,6 +8,11 @@ class TestAccount:
         student_obj = mixer.blend('account.User')
         assert student_obj.pk == 1, 'Should save an instance'
 
+    def test_user_is_soft_deleted(self):
+        user_obj = mixer.blend('account.User',is_deleted = False)
+        user_obj.soft_delete()
+        assert user_obj.is_deleted == True
+
 @pytest.mark.django_db
 class TestRole:
     def test_model(self):
