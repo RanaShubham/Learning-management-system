b5325101c88e09682404d7ddd165ac23412ffd1e
anamfazal <anamfazal94@gmail.com>
2021-01-29 22:58:38 +0530

    [Anam Fazal]Refactored:each endpoint to have separate class

--- LMS/mentor/urls.py
+++ LMS/mentor/urls.py
@@ -6,6 +6,6 @@ app_name = "mentor"
 
 urlpatterns = [
     path('users', AdminView.as_view(), name="get_mentors"),
-    path('user', MentorProfile.as_view(), name="post_mentor"),
+    path('user', CreateMentor.as_view(), name="post_mentor"),
     path('user/<int:pk>', MentorProfile.as_view(), name="get_delete_mentor"),
 ]
