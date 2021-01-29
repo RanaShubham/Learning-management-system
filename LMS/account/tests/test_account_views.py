6cadbb998b5d7bd580e594ddc9b1aca0541fe49b
anamfazal <anamfazal94@gmail.com>
2021-01-29 22:57:03 +0530

    [Anam Fazal]Refactored:added new tests

--- LMS/account/tests/test_account_views.py
+++ LMS/account/tests/test_account_views.py
@@ -20,11 +20,16 @@ class Data(TestCase):
         this method setup all the url and data which was required by all test cases
         """
         User.objects.create_superuser(name="admin", email="admin@email.com", phone_number="1234567890", password="adminpass")
+
+
         self.register_url = reverse("account:post_user")
         self.get_url = reverse("account:get_users")
         self.login_url = reverse("account:login_user")
+        self.logout_url = reverse("account:logout_user")
         self.patch_url = reverse("account:update_delete_user", kwargs={'pk':'1'})
         self.admin_data = {'email': "admin@email.com", 'password': "adminpass"}
+
+
         self.valid_registration_data = {'name': "adam",
                                         'role': "admin",
                                         'email': "adam@gmail.com",
@@ -46,7 +51,14 @@ class Data(TestCase):
 
     def test_login_admin(self):
         response = self.client.post(self.login_url, self.admin_data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK) 
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_logout_admin(self):
+        response=self.client.post(self.login_url, self.admin_data, format='json')
+        Authorization = response.get('HTTP_AUTHORIZATION')
+        response = self.client.get(self.logout_url, HTTP_AUTHORIZATION = Authorization, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
     def test_register_demo(self):
         response = self.client.post(self.login_url, self.admin_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -67,6 +79,13 @@ class Data(TestCase):
         response = client.patch(self.patch_url, self.valid_patch_data, HTTP_AUTHORIZATION = Authorization,format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+    def test_delete_user(self):
+        response = self.client.post(self.login_url, self.admin_data, format='json')
+        Authorization= response.get('HTTP_AUTHORIZATION')
+        self.client.post(self.register_url, self.valid_registration_data, HTTP_AUTHORIZATION=Authorization,
+                                    format='json')
+        response = self.client.delete(self.patch_url,HTTP_AUTHORIZATION=Authorization, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_invalid_login_admin_returns_401_UNAUTHORIZED(self):
         response = self.client.post(self.login_url, self.invalid_login_data_invalid_credentials, format='json')
@@ -86,3 +105,11 @@ class Data(TestCase):
         client = APIClient()
         response = client.patch(self.patch_url, self.valid_patch_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_invalid_token_returns_400_BAD_REQUEST(self):  # DECODE ERROR
+        response = self.client.post(self.login_url, self.admin_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        Authorization = response.get('HTTP_AUTHORIZATION').replace("m","M")
+        response = self.client.get(self.get_url, HTTP_AUTHORIZATION=Authorization, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
