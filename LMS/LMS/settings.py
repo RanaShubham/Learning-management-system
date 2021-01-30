189870d1d66dc980ca3b98ba2bf99f3c755d2da1
b115030 <iiitiangokul@gmail.com>
2021-01-30 16:46:12 +0530

    [Gopinath]Refactored: database settings to connect to EC2 PostgreSQL

--- LMS/LMS/settings.py
+++ LMS/LMS/settings.py
@@ -88,8 +88,11 @@ AUTHENTICATION_BACKENDS = (
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'POST': '',
     }
 }
 
