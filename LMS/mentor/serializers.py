ab453193f1e4286f09bab9161f0279ad0b643540
Shubham Rana <ranashubham7296@gmail.com>
2021-02-04 11:43:10 +0530

    [Shubham Rana] Refactored: to improve readablity.

--- LMS/mentor/serializers.py
+++ LMS/mentor/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from .models import Mentor
-
 User = get_user_model()
 
 
@@ -9,12 +8,6 @@ class MentorSerializer(serializers.ModelSerializer):
     """
     this class is used for serialization and deserialization of mentor details
     """
-
     class Meta:
         model = Mentor
         fields = ['course','user', 'id']
-
-
-
-
-
